cadf65724d469094413e09e462da9fb236f31fae
norielagartosacristan <norielagartosacristan@gmail.com>
2026-07-03 06:51:45 +0800

    New edit

--- installer/postgres.sh
+++ installer/postgres.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
-echo "Installing PostgreSQL..."
+set -e
+
+echo "========================================"
+echo " Installing PostgreSQL"
+echo "========================================"
+
+sudo apt update
 
 sudo apt install -y postgresql postgresql-contrib
 
 sudo systemctl enable postgresql
 
 sudo systemctl start postgresql
+
+echo ""
+echo "Checking PostgreSQL..."
+
+sudo systemctl status postgresql --no-pager
+
+echo ""
+echo "Creating SkyGrid Database..."
+
+sudo -u postgres psql <<EOF
+CREATE DATABASE skygrid_vendo;
+EOF
+
+echo ""
+echo "========================================"
+echo " PostgreSQL Installed Successfully"
+echo "========================================"
+
+psql --version
