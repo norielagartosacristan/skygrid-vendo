6eb66903c13007a78cb4199c4a0129a66673a9dd
Noriel Sacristan <norielagartosacristan@gmail.com>
2026-07-09 22:17:09 +0000

    My local changes

--- server/dist/app.js
+++ server/dist/app.js
@@ -25,7 +25,6 @@ const package_routes_1 = __importDefault(require("./modules/package/routes/packa
 const machine_routes_1 = __importDefault(require("./modules/machine/routes/machine.routes"));
 const error_middleware_1 = require("./middleware/error.middleware");
 const coin_routes_1 = __importDefault(require("./modules/payment/coin/coin.routes"));
-const package_routes_2 = __importDefault(require("./routes/package.routes"));
 const app = (0, express_1.default)();
 const CLIENT_BUILD_PATH = path_1.default.resolve(process.cwd(), "client/dist");
 app.set("trust proxy", true);
@@ -50,7 +49,7 @@ app.use("/api/captive", captive_routes_1.default);
 app.use("/api/vouchers", voucher_routes_1.default);
 app.use("/api/packages", package_routes_1.default);
 app.use("/api/machine", machine_routes_1.default);
-package_routes_2.default.use("/coin", coin_routes_1.default);
+app.use("/api/coin", coin_routes_1.default);
 /* =========================
    STATIC FRONTEND
 ========================= */
