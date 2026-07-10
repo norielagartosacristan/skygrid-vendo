44e0495ab0add4a674d2db732b9b90aaa1cc8045
Noriel Sacristan <norielagartosacristan@gmail.com>
2026-07-10 13:54:25 +0000

    My local changes

--- server/dist/modules/network/websocket/network.socket.js
+++ server/dist/modules/network/websocket/network.socket.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.networkSocket = void 0;
 const ws_1 = require("ws");
-const networkMonitor_service_1 = require("../services/networkMonitor.service");
 class NetworkSocket {
     wss;
     init(server) {
@@ -12,9 +11,10 @@ class NetworkSocket {
         });
         this.wss.on("connection", (ws) => {
             console.log("📡 Network Dashboard Connected");
-            ws.send(JSON.stringify(networkMonitor_service_1.networkMonitor.getData()));
-            ws.on("close", () => {
+            ws.on("close", (code, reason) => {
                 console.log("📡 Network Dashboard Disconnected");
+                console.log("Code:", code);
+                console.log("Reason:", reason.toString());
             });
         });
     }
