4e73427a17ffa5403a62894627ed07a3dc31da32
Noriel Sacristan <norielagartosacristan@gmail.com>
2026-07-10 10:33:40 +0000

    My local changes

--- server/dist/modules/payment/coin/coin.service.js
+++ server/dist/modules/payment/coin/coin.service.js
@@ -6,20 +6,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.coinService = void 0;
 const prisma_1 = __importDefault(require("../../../config/prisma"));
 const session_service_1 = require("../../captive/session/session.service");
+const time_1 = require("../../../utils/time");
+const machine_service_1 = require("../../machine/services/machine.service");
 class CoinService {
-    async insertCoin(machineId, clientMac, clientIP, amount) {
+    async insertCoin(data) {
+        const { clientMac, clientIP, amount } = data;
         console.log("========== COIN INSERTED ==========");
+        //console.log("Machine:", machineId);
+        console.log("Client:", clientIP);
         console.log("Amount:", amount);
         const pkg = await prisma_1.default.package.findFirst({
             where: {
-                price: amount,
+                price: Number(amount),
                 isActive: true
             }
         });
         if (!pkg) {
             throw new Error(`No package configured for ₱${amount}`);
         }
-        const session = await session_service_1.sessionService.createSession(machineId, pkg.id, clientMac, clientIP, pkg.duration);
+        const machine = await machine_service_1.machineService.getCurrentMachine();
+        if (!machine) {
+            throw new Error("Machine not registered.");
+        }
+        const machineId = machine.id;
+        const session = await session_service_1.sessionService.createSession(machineId, pkg.id, clientMac, clientIP, (0, time_1.convertToMinutes)(pkg.duration, pkg.durationUnit));
         await prisma_1.default.coinTransaction.create({
             data: {
                 machineId,
